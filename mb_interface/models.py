b6be0a20e2e342075dcbe6358f5e5fe96aa84ad4
Daniel <dbalcells@gmail.com>
2017-10-14 23:57:00 +0000

    Making MBID primary key for Songs

--- mb_interface/models.py
+++ mb_interface/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Song(models.Model):
     artist = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
-    mbid = models.CharField(max_length=50)
+    mbid = models.CharField(max_length=50, primary_key=True)
     album = models.CharField(max_length=200)
 
     def __str__(self):
