e23c0e995d73784fc9baa4193743ad8c148c200f
Daniel <dbalcells@gmail.com>
2017-10-15 16:17:32 +0000

    Added check for duplicate search results

--- mb_interface/search.py
+++ mb_interface/search.py
@@ -7,9 +7,11 @@ mbn.set_useragent('Notella','dev')
 # Queries MB API for a given song title, returns a list of results
 def search_song_by_title(song_query, limit=20):
     try:
-        query_results = mbn.search_recordings(query=song_query, limit=limit)
+        # Oversearch, prune later to remove duplicates
+        query_results = mbn.search_recordings(query=song_query, limit=2*limit)
         result_list = query_results['recording-list']
         song_list = []
+        song_string_list = []
         for result in result_list:
             title = result['title']
             artist = result['artist-credit-phrase']
@@ -19,8 +21,10 @@ def search_song_by_title(song_query, limit=20):
                         artist=artist,
                         mbid=mbid,
                         album=album)
-            song.save()
-            song_list.append(song)
-        return song_list
+            if str(song) not in song_string_list:
+                song_string_list.append(str(song))
+                song.save()
+                song_list.append(song)
+        return song_list[:limit]
     except (mbn.ResponseError):
         return []
