3c0b94b248ad5227fafe9987acefc43d83efa6b5
Daniel <dbalcells@gmail.com>
2017-10-15 03:22:23 +0000

    Implemented Link model

--- mb_interface/models.py
+++ mb_interface/models.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+# Basic model for a song containing artist, title, ID, and album.
 class Song(models.Model):
     artist = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
@@ -12,4 +14,18 @@ class Song(models.Model):
 
     def __str__(self):
         render_string = '"' + self.title + '", by ' + self.artist
+        return render_string.encode('utf-8')
+
+# Basic model for a link between two songs. Contains FK relations to the two
+# songs and a string explaining the connection.
+class Link(models.Model):
+    from_song = models.ForeignKey(Song, on_delete=models.CASCADE,
+                related_name='from_song')
+    to_song = models.ForeignKey(Song, on_delete=models.CASCADE,
+                related_name='to_song')
+    link_phrase = models.CharField(max_length=500)
+
+    def __str__(self):
+        render_string = str(self.from_song) + ' - ' + str(self.to_song)
+        render_string += '. ' + self.link_phrase
         return render_string.encode('utf-8')
